d5022f0338c16ea3221a71a1c9cd77cf4828c932
Alexandre Lemaire <alexandre.lemaire@laulem.com>
2026-05-10 18:52:10 +0200

    ✨ feat(reranker): update configuration for HuggingFace reranker service

--- src/main/java/com/laulem/vectopath/infra/conf/RerankerConfiguration.java
+++ src/main/java/com/laulem/vectopath/infra/conf/RerankerConfiguration.java
@@ -2,17 +2,26 @@ package com.laulem.vectopath.infra.conf;
 
 import com.laulem.vectopath.business.service.RerankerService;
 import com.laulem.vectopath.infra.properties.RerankerProperties;
+import com.laulem.vectopath.infra.service.DefaultRerankerServiceImpl;
 import com.laulem.vectopath.infra.service.HuggingFaceRerankerServiceImpl;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-@ConditionalOnProperty(name = "reranker.type", havingValue = "HUGGINGFACE")
-public class HuggingFaceRerankerConfiguration {
+public class RerankerConfiguration {
+
     @Bean
-    public RerankerService rerankerService(RerankerProperties properties) {
+    @ConditionalOnProperty(name = "reranker.type", havingValue = "HUGGINGFACE")
+    public RerankerService huggingFaceRerankerService(RerankerProperties properties) {
         return new HuggingFaceRerankerServiceImpl(RestClient.builder().baseUrl(properties.getBaseUrl()).build());
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(RerankerService.class)
+    public RerankerService defaultRerankerService() {
+        return new DefaultRerankerServiceImpl();
     }
 }
